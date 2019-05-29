f97c4a3a82b293bf26a67fcbebf7d99e3a103d94
public <public>
2019-05-29 17:25:16 +0800

    autocommit

--- build/gitupload.js
+++ build/gitupload.js
@@ -1,28 +1,4 @@
-/*
 
-var shell = require('shelljs/global')
-var res = exec('git status -s').stdout
-console.log('开始转换 git status')
-
-var arrayResult = res.split('\n');
-
-var array = []
-console.log('开始筛选 git status')
-
-array = arrayResult.filter((item) => {
-    var i = item.trim()
-    if(i[0] === 'M' || i[0] === 'A') {
-        return true
-    }
-    return false
-})
-var host = 'https://github.com/zhouping920819/vant-H5.git'
-console.log('开始转换 上传:',host)
-array.forEach((item) => {
-    var i = item.trim().replace(/^m|^a/i,'').trim()
-    exec(i)
-})
-*/
 //局部模式
 var shell = require('shelljs');
 //全局模式下，就不需要用shell开头了。
